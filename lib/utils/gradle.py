6beb2c3049d21db5e505495870fc24f6b8912c5d
srishanbhattarai <srishanbhattarai@gmail.com>
2017-08-19 15:07:55 +0545

    (feat) Add stderr prints for RN gradle builds

--- lib/utils/gradle.py
+++ lib/utils/gradle.py
@@ -3,8 +3,10 @@ from subprocess import Popen, PIPE
 def build_react_native():
     with Popen( ['./android/gradlew', '-p', 'android', 'assembleRelease'], bufsize=-1, stdout=PIPE, stderr=PIPE,
                universal_newlines=True) as process:
-        # for line in process.stdout:
-        #     pass
-        pass
+        for line in process.stdout:
+             print(line)
+
+        for line in process.stderr:
+            print(line)
 
     return process.returncode
