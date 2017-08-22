92e4ac95606a6e01ad3792b7596cd0065301c46a
srishanbhattarai <srishanbhattarai@gmail.com>
2017-08-22 16:54:27 +0545

    (feat) Add project registration methods to register service

--- lib/services/registration_service.py
+++ lib/services/registration_service.py
@@ -1,18 +1,40 @@
 import sys
+
 from lib.services.stdio_service import get_login_credentials
+from lib.utils.gradle import get_react_native_project_name
 
 class RegistrationService():
 
     def __init__(
         self,
-        auth_service_instance
+        auth_service_instance,
+        storage_instance
     ):
         self.auth = auth_service_instance
+        self.storage = storage_instance
+
 
     def delegate(self, is_user_registration=False):
         ''' Delegate for the CLI. The only public method. '''
         if is_user_registration:
             self.__create_user_with_email__()
+            sys.exit(0)
+
+        self.__register_project__()
+
+
+    def __register_project__(pass):
+        ''' Register a project on the server. '''
+        proj_name = get_react_native_project_name()
+        data = {
+            'name': proj_name
+        }
+
+        self.storage.register_project(self.__compose_project_output_path__(proj_name), data)
+
+
+    def __compose_project_output_path__(self, proj_name):
+        return proj_name
 
 
     def __create_user_with_email__(self):
