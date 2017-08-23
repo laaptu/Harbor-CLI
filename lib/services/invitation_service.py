195599e2f432319321c03ca5e8b927d42901e91c
srishanbhattarai <srishanbhattarai@gmail.com>
2017-08-23 16:51:34 +0545

    (feat) Add invitation service hooks to add a user to a project

--- lib/services/invitation_service.py
+++ lib/services/invitation_service.py
@@ -1,31 +1,36 @@
 import sys
 
 from lib.utils.gradle import get_react_native_project_name
-from lib.exceptions.FileNotFound import FileNotFoundException
-from lib.exceptions.UserNotFound import UserNotFoundException
 
 class InvitationService():
 
-    def __init__(self, email, storage_instance):
-        self.email = email
+    def __init__(self, role, email, storage_instance):
+        self.role = role
+        self.target_email = email
         self.storage = storage_instance
 
 
     def delegate(self):
         try:
             proj_name = get_react_native_project_name()
-        except FileNotFoundException as e:
+            data = self.storage.get_details_for_user_by_email(self.target_email)()
+            target_uid = data['uid']
+        except Exception as e:
             print(e.message)
             sys.exit(1)
 
-        try:
-            data = self.storage.get_details_for_user_by_email(self.email)()
-            print('data:: ', data)
-        except UserNotFoundException as e:
-            print(e.message)
-            sys.exit(1)
+        proj_members_path = self.__compose_member_output_path__(proj_name)
+        user_details = {
+            target_uid: {
+                'role': self.role,
+                'notificationLevel': 'all'
+            }
+        }
+
+        self.storage.add_user_to_project(proj_members_path, user_details)
+        print('Invited "{0}" to "{1}" as "{2}"'.format(self.target_email, proj_name, self.role))
 
 
-    def __compose_member_output_path(self, proj_name):
+    def __compose_member_output_path__(self, proj_name):
         ''' Returns the database  path for new uploads to a project. '''
-        return 'members' + '/' + ''.join(proj_name.split('.'))
+        return ''.join(proj_name.split('.'))
