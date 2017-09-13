84d3b82fe9967fa7c110b76d01f325a13d0b915b
srishanbhattarai <srishanbhattarai@gmail.com>
2017-09-13 14:00:56 +0545

    (feat) Add post deploy plugin hook for HipChat

--- lib/plugins/hipchat.py
+++ lib/plugins/hipchat.py
@@ -15,39 +15,65 @@ class HipChatPlugin():
     def __init__(self):
         self.hipchat_url = "https://{0}.hipchat.com/v2/room/{1}/notification?auth_token={2}"
         self.deploying_message = "{0} is deploying a {1} build from branch {2}."
-        self.deployed_message = "{0} deployed a {1} build from branch {2}. See the changelog {3}."
+        self.deployed_message = "{0} deployed a {1} {2} from branch {3}. See the changelog {4}."
 
     def apply(self, compiler):
         ''' Register plugins. '''
         compiler.plugin('deploy/will_upload', self.pre_deploy)
-        compiler.plugin('deploy/did_deploy', self.did_deploy)
+        compiler.plugin('deploy/did_deploy', self.post_deploy)
 
-    def pre_deploy(self, compilation):
-        ''' Send a notification before deploying. '''
-        # hipchatdetails = destructure(compilation)('hipchatdetails')
+    def config_data(self):
+        ''' Returns hipchat config data. '''
         if not config.is_hipchat_configured():
             return
-        release_type, branch = destructure(compilation)(
-            'release_type', 'branch'
-        )
 
+        return config.get()['hipchat']
+
+    def notify(self, payload):
+        ''' Send a notification. '''
         hipchat_details = config.get()['hipchat']
         company_name, room_id, auth_token = destructure(hipchat_details)(
             'company_name', 'room_id', 'auth_token'
         )
+        requests.post(
+            self.hipchat_url.format(company_name, room_id, auth_token),
+            data=payload
+        )
+
+    def get_build_link(self, url):
+        ''' Return an <a> tag with the build link. '''
+        link = "<a href={0}>build</a>"
+        return link.format(url)
+
+    def pre_deploy(self, compilation):
+        ''' Send a notification before deploying. '''
+        if not config.is_hipchat_configured():
+            return
+
+        release_type, branch = destructure(compilation)('release_type', 'branch')
         notification_data = {
-            'color': "green",
+            'color': 'red',
             'message': self.deploying_message.format(
                 git.whoami(), release_type, branch
             ),
             'notify': True,
             'message_format': "html"
         }
-        requests.post(
-            self.hipchat_url.format(company_name, room_id, auth_token),
-            data=notification_data
-        )
+        self.notify(notification_data)
 
-    def did_deploy(self, compilation):
+    def post_deploy(self, compilation):
         ''' Send a notification after deploying. '''
-        pass
+        if not config.is_hipchat_configured():
+            return
+
+        release_type, branch, url = destructure(compilation)('release_type', 'branch', 'url')
+        notification_data = {
+            'color': "green",
+            'message': self.deployed_message.format(
+                git.whoami(), release_type, self.get_build_link(url), branch
+            ),
+            'notify': True,
+            'message_format': "html"
+        }
+        self.notify(notification_data)
+
