694f1ffe4a25ea9c03a668023a2653db53189b26
Srishan Bhattarai <srishanbhattarai@gmail.com>
2017-10-20 21:09:41 +0545

    (refactor) Registration service for user and project. Better icon discovery

--- lib/commands/register.py
+++ lib/commands/register.py
@@ -1,10 +1,12 @@
 ''' Service for register command. '''
 import sys
+from requests import exceptions
 
+from lib import android
 from lib.anchor import Anchor
 from lib.logger import logger
-from lib.utils.validators import is_valid_email
 from lib.inqurires import getlogincredentials
+from lib.utils.validators import is_valid_email
 from lib.services.firebase_service import Firebase
 
 class Register(Anchor):
@@ -15,10 +17,19 @@ class Register(Anchor):
     def __init__(self, user_registration):
         super().__init__()
 
+        self.user = None
+        self.icon_url = None
+        self.icon_path = None
+        self.packagename = None
+        self.projectdetails = None
         self.is_user_registration = user_registration or False
 
     def execute(self):
         ''' Register a user or a project. '''
+        if not android.is_android():
+            logger().error('You are not in a valid Android project.')
+            sys.exit(1)
+
         if self.is_user_registration:
             self.register_user()
         else:
@@ -35,8 +46,7 @@ class Register(Anchor):
 
         try:
             Firebase().signup_via_email(email, password)
-        except Exception as e: #pylint: disable=broad-except
-            print('error = ', e)
+        except Exception: #pylint: disable=broad-except
             logger().error('Cannot register this email. Please try another one.')
             sys.exit(1)
 
@@ -46,4 +56,82 @@ class Register(Anchor):
 
     def register_project(self):
         ''' Register a project. '''
-        logger().info('Register project.')
+
+        email, password = getlogincredentials()
+
+        try:
+            Firebase().login_with_email(email, password)
+            self.user = Firebase().get_current_user_details()
+        except exceptions.HTTPError:
+            logger().error('Cannot login. Please verify credentials.')
+            sys.exit(1)
+
+        self.projectdetails = android.project_details()
+        self.packagename = ''.join(self.projectdetails['packagename'].split('.'))
+
+        # local path to ic_launcher icon
+        self.icon_path = android.launcher_icon()
+
+        if self.icon_path is None:
+            logger().warning('Cannot find any valid icons.')
+        else:
+            self.icon_url = self.upload_icon()
+
+        if self.already_exists():
+            logger().error('This project has already been registered.')
+            sys.exit(1)
+
+        self.update_database()
+
+        success = '"{0}" registered successfully.'.format(
+            self.projectdetails['name'] or self.packagename
+        )
+
+        logger().info(success)
+
+    def update_database(self):
+        ''' Update database for new project. '''
+        projectpath = 'projects/{packagename}'
+        project = {
+            'uploads': {},
+            'metadata': {},
+            'iconUrl': self.icon_url,
+            'packageName': self.packagename,
+            'name': self.projectdetails['name'] or self.packagename
+        }
+
+        # The one registering the project is an admin by default.
+        memberpath = 'members/{uid}'
+        member = {
+            'role': 'admin',
+            'notificationLevel': 'all'
+        }
+
+        Firebase().write_to_db(
+            projectpath.format(packagename=self.packagename),
+            project
+        )
+
+        Firebase().write_to_db(
+            memberpath.format(uid=self.user['uid']),
+            member
+        )
+
+
+    def already_exists(self):
+        ''' Returns True if a project by the same package name already exists. '''
+        path = 'projects/{0}'
+        data = Firebase().get_from_db(
+            path.format(self.packagename)
+        )
+
+        return data.val() is not None
+
+    def upload_icon(self):
+        ''' Upload an icon and get its download url. '''
+        path = '{0}/icon.png'
+
+        return Firebase().upload(
+            path.format(self.packagename),
+            self.icon_path
+        )
